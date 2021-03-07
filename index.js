dcabe121e7736ff9defee9b1c589c24429da98c0
MD. Khaled Zilani Rajon <43389600+kzrajon@users.noreply.github.com>
2021-03-07 10:29:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,26 +18,32 @@ const path = require('path');
 
 
 
-
+app.use(cors());
 app.use(bodyParser.json({limit:'1000mb',extended:true}));
 app.use(bodyParser.urlencoded({limit:'1000mb',extended: true}));
 app.use(cookieParser());
 app.use(helmet());
-app.use(fileUpload());
-app.use(cors());
+app.use(fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    useTempFiles : true,
+    tempFileDir : '/tmp/',
+    preserveExtension: true
+}));
 
-app.post('/user-resume',function(req,res,next){
+
+app.post('/upload',function(req,res,next){
 	
 	var accessToken = req.query.access_token?req.query.access_token:null;
 	var IP = req.query.ip?req.query.ip:null;
 	
 	var resumeFile = req.files.resume ;
-       //console.log(resumeFile);
-        var filename = uuid4()+'.pdf';
+	var ext = resumeFile.name.substr(resumeFile.name.length-3);
+      
+        var filename = uuid4()+ext;
 	resumeFile.mv(path.resolve(__dirname+'/resume',filename), function(err) {
 	    if (err){
 	      return res.status(500).json(err);
-	     console.log("error",err);
+	    
 	   }
     
     const resume = new resumeParser(path.resolve(__dirname+'/resume',filename));
@@ -47,7 +53,6 @@ app.post('/user-resume',function(req,res,next){
 				if(accessToken){
 					return service.saveResume(data,accessToken)
 						.then((r)=>{
-							//console.log("r",r);
 							return res.status(r.status).json({notification:"Your password already E-mail to you. Please fillup your resume."});
 						})
 						.catch(e=>e);
@@ -55,26 +60,21 @@ app.post('/user-resume',function(req,res,next){
 				else{
 					return service.userRegister(data.parts.email)
 						.then((rr)=>{
-							//console.log("after regis",rr);
 							if(rr.status == 201){
 								return service.userLogin(data.parts.email,IP)
 								 .then((rrr)=>{
-									 //console.log(rrr);
 									let accessToken = rrr.data.access_token;
 									 return service.saveResume(data,rrr.data.access_token)
 										.then((r)=>{
-											//console.log("r",r);
 											return res.status(r.status).json({access_token:accessToken,notification:"Your password already E-mail to you. Please fillup your resume."});
 										})
 										.catch((e)=>{
-		//									console.log(e);
 											return res.status(e.response.status).send(e.response.data);
 											
 										});
 								 })
 								 .catch(ee=>{
-			//						 console.log(ee);
-											return res.status(ee.response.status).send(ee.response.data);
+									return res.status(ee.response.status).send(ee.response.data);
 								 });
 							}
 						})
@@ -82,21 +82,18 @@ app.post('/user-resume',function(req,res,next){
 							return res.status(er.response.status).json("You are already registrared User. please sign in and upload your resume again.");
 						});
 				}
-				//console.log('pdf data ', data);
-				//console.log("email",data.parts.email);
-				//res.status(200).send(data);
+				
 			})
 			.catch(error => {
-				//console.error(error);
 				return res.status(500).json("Resume can not parsable");
 			});
-			//res.end();
+		
   });
 });
 
 
 app.get('/',function(req,res,next){
-	return res.status(200).json("Please upload your resume in '/user-resume' URI with POST request.");
+	return res.status(200).json("Please upload your resume in '/upload' URI with POST request.");
 });
 
 app.listen(PORT,()=>{
